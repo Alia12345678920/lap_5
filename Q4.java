9d56ca46d8d639761b053ad7d1ecfedf58f2e485
Alia12345678920 <111192996+Alia12345678920@users.noreply.github.com>
2022-09-22 23:19:54 +0300

    Add files via upload

---
+++ Q4.java
@@ -0,0 +1,14 @@
+public class Q4{
+
+    public static void main(String[] args) {
+        QQ4 alia=new QQ4(1000);
+        alia.addInsert(10);
+        System.out.println("Resulting balance="+alia.getBalance());
+        System.out.println("Expected result="+1100);
+
+    }
+
+
+
+}
+
